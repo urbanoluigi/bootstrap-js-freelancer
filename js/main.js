fc67a7054e9f348de31913c88f7f500b87e0510c
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-27 11:44:29 +0100

    aggiunto preventdefault

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 // Array contenente i codici promozionali validi
 let promoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 
+function calculateOffer(event) {
+    // Previeni il comportamento predefinito del form
+    event.preventDefault();
