ea9b6225ea688f6328cab0504e3a7d9bbbc53d43
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-27 10:56:36 +0100

    orari definiti, tipi di lavoro aggiunti e codici promo aggiunti

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ function calculateOffer(event) {
     var hoursRequested = parseFloat(document.getElementById('hours').value);
     var typeOfWork = document.getElementById('type-of-work').value;
     var discountCode = document.getElementById('discount-code').value;
+    
+    // Definisci i prezzi orari
+    var hourlyRates = {
+        '1': 20.50, // Backend Development
+        '2': 15.30, // Frontend Development
+        '3': 33.60  // Analisi Progettuale
+    };
+
+    // Controlla se il tipo di lavoro è valido
+    if (!(typeOfWork in hourlyRates)) {
+        alert('Seleziona un tipo di lavoro valido.');
+        return;
+    }
+
+    // Calcola il prezzo totale
+    var totalPrice = hourlyRates[typeOfWork] * hoursRequested;
+
+    // Controlla se il codice promozionale è valido
+    var validPromoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+    var usedPromoCodes = [];
