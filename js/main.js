4dc900af4754667b0fe19bbc0723ee95c0c57a84
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-27 11:46:02 +0100

    prendiamo tramite id gli elementi che ci servono

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ let promoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 function calculateOffer(event) {
     // Previeni il comportamento predefinito del form
     event.preventDefault();
+
+    // Ottieni i valori inseriti dall'utente
+    let hoursRequested = parseFloat(document.getElementById('hours').value);
+    let projectType = document.getElementById('type-of-work').value;
+    let discountCode = document.getElementById('discount-code').value;
